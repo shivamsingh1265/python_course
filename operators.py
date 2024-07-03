9469040d40599aa599e16cedabd81f7b7b234421
shivamsingh1265 <shivamsinghjan399@gmail.com>
2024-07-03 08:46:11 +0000

    add operators file

--- operators.py
+++ operators.py
@@ -10,3 +10,11 @@ print(5**2)     #exponention
 print(5//2)    #Floor division
 
 #Assignment operator
+
+#Logical operator
+print(not False)
+print(not True)
+
+  input("enter your name:")
+ print("welcome",name)
+    
--- stringC.py
+++ stringC.py
@@ -6,6 +6,13 @@ print("It's alright")
 print("He is called 'Johnny'")
 print('He is called "Johnny"')
 
+ #two string add in a single variable is called concatenation.
  
+ #"hello"+"world" = "helloworld" 
  
+ str1 ="hello"
+ str2 ="world"
+ print(str1+str2)
+ 
+#indexing is help to access character
  
